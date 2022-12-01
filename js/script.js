a892bdfe0f6422f644b7f315e1a5366ec38a30d3
leoncci <leon.morival@gmail.com>
2022-12-01 13:03:25 +1100

    commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
-
-
 myID = document.getElementById("back-to-top");
 
-var myScrollFunc = function () {
+window.addEventListener("scroll", function () {
     var y = window.scrollY;
     if (y >= 50) {
         myID.setAttribute("style", "display: block")
     } else {
         myID.setAttribute("style", "display: none")
     }
-};
-
-window.addEventListener("scroll", myScrollFunc);
+});
